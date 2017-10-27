796c924eeb545246da542eacc9b4a2971c58fd0d
Alejandro Martinez <alejandro.martinez@utrgv.edu>
2017-10-27 14:46:29 -0500

    performed exhaustive feature selection

--- wine_classification.py
+++ wine_classification.py
@@ -17,6 +17,12 @@ from sklearn.svm import SVC
 
 import itertools
 
+def findsubsets(S):
+    subsets=[];
+    for subsets_of_given_size in [list(itertools.combinations(S, m)) for m in range(1,len(S)+1)]:
+        for subset in subsets_of_given_size:
+            subsets.append(list(subset))
+    return subsets
 
 #Load data into data fram.
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
@@ -41,32 +47,6 @@ dataset = pandas.read_csv(url, names=names)
 print(dataset.corr())
 
 
-#split-out dataset for sanity
-array=dataset.values
-
-#features columns
-X=array[:,1:13]
-
-#class column
-Y=array[:,0]
-
-seed=7
-
-
-#save data for sanity check after comparing
-validation_size=0.20
-X_train, X_validation, Y_train, Y_validation=model_selection.train_test_split(X,Y, test_size=validation_size,random_state=seed)
-
-
-scoring='accuracy'
-
-#A list of the classes of models we will use.
-models = []
-models.append(('SVM', SVC()))
-
-# evaluate each model with each subset of features.
-results = [1]
-names = [1]
 
 features=['Alcohol',
 'Malicacid',
@@ -84,20 +64,48 @@ features=['Alcohol',
 
 
 
-def findsubsets(S):
-    return [subset for subset in [list(itertools.combinations(S, m)) for m in range(1,len(S)+1)]]
 
 
+subsets=findsubsets(features)
+#split-out dataset for sanity
+array=dataset.values
 
+#features columns
+X=array[:,1:13]
+
+#class column
+Y=array[:,0]
+
+seed=7
+
+
+#save data for sanity check after comparing
+#validation_size=0.20
+#X_train, X_validation, Y_train, Y_validation=model_selection.train_test_split(X,Y, test_size=validation_size,random_state=seed)
+
+
+scoring='accuracy'
+
+#A list of the classes of models we will use.
+models = []
+models.append(('SVM', SVC()))
+
+# evaluate each model with each subset of features.
+results = []
+names = []
+
+Y=dataset['class']
 
 for name, model in models:
     for selected in subsets:
+        X=dataset[selected].values
+                 
         kfold = model_selection.KFold(n_splits=10, random_state=seed)
-        cv_results=model_selection.cross_val_score(model, X_train,Y_train, cv=kfold, scoring=scoring)
+        cv_results=model_selection.cross_val_score(model, X,Y, cv=kfold, scoring=scoring)
         results.append(cv_results)
         names.append(name)
         msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-        print(msg)
+        #print(msg)
 
 # Compare Algorithms
 fig = plt.figure()
