a2c9d1ee20fa0ffd85fb5b1f4e89f87e6062d132
Alejandro Martinez <mnight08@yahoo.com>
2017-10-27 18:31:11 -0500

    started working with grouping titanic data into dead, and survivors to analyze data

--- titanic/titanic_survival_classifier.py
+++ titanic/titanic_survival_classifier.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 27 15:57:35 2017
-This is our first submission of the titanic classifcation problem 
+This is our first submission of the titanic classifcation problem.  
+
+Goal is to classify the test data as survived or died 
+based on  labeled training data. The columns are.
+'PassengerId',	'Survived',	'Pclass',	'Name',	'Sex',	
+'Age',	'SibSp',	'Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked'
+
+Categorical Variables:
+'PassengerId', 'Survived', 'Ticket', 'Cabin',	'Embarked'
+
+
+Quantitative Variables:
+'Age',	'SibSp',	'Parch', 'Fare',	
+
 @author: vpx365
 """
 
 
-import pandas
+import pandas 
 from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
@@ -25,7 +38,45 @@ seed=7
 
 #Load data into data fram.
 filename="train.csv"
-names = ['PassengerId',	'Survived',	'Pclass',	'Name',	'Sex',	
-         'Age',	'SibSp',	'Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked']
-dataset = pandas.read_csv(filename, names=names)
+#column names are auto filled. Columns are  ['PassengerId',	'Survived',	'Pclass',	'Name',	'Sex',	
+#        'Age',	'SibSp',	'Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked']
+dataset = pandas.read_csv(filename)
+
+#Numerical summary of the data.  Some ages are missing.  Need to decide how to fill in.
+print(dataset.describe())
+
+
+#Analyze relationship between survival and other variables
+grouped_survivors=dataset.groupby('Survived');
+
+
+#Return Series with number of non-NA/null observations over requested axis
+print(grouped_survivors.count())
+
+
+grouped_sex=dataset.groupby('Sex');
+
+print(grouped_sex.count())
+
+#Numerical summary of the groups.
+#categorical variables
+#print(grouped_survivors['Sex'].count())
+#print(grouped_survivors['Pclass'].count())
+
+#print(grouped_survivors['Name'].count())
+
+#print(grouped_survivors['Ticket'].count())
+
+#print(grouped_survivors['Cabin'].count())
+
+#print(grouped_survivors['Embarked'].count())
+
+#quantitative variables
+#print(grouped_survivors['SibSp'].mean())
+#print(grouped_survivors['Parch'].mean())
+
+
+
+
+#Visualization for the groups.
 
