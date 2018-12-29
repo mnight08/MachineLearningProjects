65ab1d79cd0c5522a06c513e8f6d3084b88b7d09
Alejandro Martinez <alejandro.martinez@utrgv.edu>
2018-12-29 06:17:13 -0600

    added some basic plotting and data loading functionality

--- VSB_Partial_Discharge_Detection_Due_March_2018/datamanager.py
+++ VSB_Partial_Discharge_Detection_Due_March_2018/datamanager.py
@@ -1,46 +1,135 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 13 12:27:32 2017
-
+"""Manages access to the underlying data.
+Created on Fri December 28 9:07 pm
 @author: vpx365
 """
-
-
-
- # Load libraries
-import pandas
-from pandas.plotting import scatter_matrix
+import os
+import functools
+import itertools
+import pandas as pd
+import numpy as np
+import pyarrow.parquet as pq
+from scipy import signal
 import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
-import seaborn as sns
+class DataManager:
+    """Manages access to the underlying data.
+    Example:
+    A datamanager is responsible for all access to data.
+    The methods are of the form:
+    load_{what to load}(args)
+    get_{what to get}(args)
+    make_{what to make}(args)
+    load methods pull data into memory.  get methods return copies of tables
+    loaded from disk,
+    or loads tables if they are missing.  make methods generate content that is
+    saved into the build folder.
+    takes a data set, loads it from disk, caches the data, and
+    returns dat  data frame for the given year from the events data.
+    """
+    def __init__(self):
+        self.load_path = "../../Learning_Data/VSB_Partial_Discharge_Detection_Due_March_2018/all/"
+        self.make_path = "../../Machine_Learning_Artifact/VSB/"
+        self.train=None
+        self.test=None
+    
+    def load_signals(self, ids=None):
+        if isinstance(ids, int):
+            self.train=pq.read_pandas(self.load_path+"train.parquet",columns=[str(ids)]).to_pandas()
+            
+        elif isinstance(ids, list):
+            self.train=pq.read_pandas(self.load_path+"train.parquet",columns=[str(i) for i in ids]).to_pandas()
+        else:
+            self.train=pq.read_pandas(self.load_path+"train.parquet").to_pandas()
+        
+                   
+        
+    
+    
+    def plot_spectogram(self, id):
+        x = np.linspace(0,1/50,80000)
+        f, t, Sxx = signal.spectrogram(x, self.train.iloc[:,id])
+        plt.pcolormesh(t, f, Sxx)
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+        plt.show()
+    
+    
+    def plot_signal(self, id):
+         x = np.linspace(0,1/50,80000)
+         plt.plot(x,self.train.iloc[:,3])         
+         plt.ylabel('Voltage')
+         plt.xlabel('Time [sec]')
+         plt.show()
+    
+    
+    '''
+    Create the training data for our model
+    '''
+    @functools.lru_cache(maxsize=32)
+    def make_training_data_year(self, year, period="All", teams = None):
+        '''
+        Make x_train and y_train consisting of game features, and labels of whether
+        team 1 won the game or not.  To fit the format of kaggle submission,
+        the smaller id will always be team 1.  Currently the games are built from
+        the playbyplay data.  It is likely more efficient to use one of the game summary files.
+        That said, if we want to extract event specefic features, then we would still need to
+        load the event data.
+        '''
+        #generate the training data that does not depend on pbp results.
+        td = self.get_games(year, period)
+    
+        #make team specific columns.        
+        #Number of division 1 years prior to the current season a game is playedin
+        td=self.get_num_div1_years_team(td)
 
+       
+        #(time  outs  +  dead  rebounds  prior  to  the  current  game  in  the  last  2years)/number of games in last 2 years
+        #td=self.get_team_type_events(td, years=2)
+        
+        # Tourney win ratio (wins/games played )
+        td=self.get_winning_rate(td, who="Team", when ="Tourney")
+        #Regular season win ratio(wins/games played)
+        #td=self.get_winning_rate(td, who="Team", when ="Regular")
 
-# Load dataset
-path = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-
-
- # shape
-print(dataset.shape)
-
-# head
-#print(dataset.head(20))
+        #turnovers/game in last 2 years.
+        #td=self.get_tournovers_team(td, who="Team", when ="Tourney")
  
-# descriptions
-print(dataset.describe())
+
+
+        #make coach columns.
+        td = self.get_num_div1_years_coach(td)
+        #wins/total played in previous three seasons.
+        
+        #td=self.get_win_rate_coach(td, period="All")
+        #td=self.get_win_rate_coach(td, period="Tourney")
 
 
 
-# histograms of the variables
-#dataset.hist()
-#plt.show()
+        #make player derived columns.
+        
+        #Average player points per attempt in last 2 years
+        #td=self.get_player_points(td)
+        #Average years a player has played.
+        #td=self.get_num_div1_years_player(td)
+
+        
+        #Average player dynamic
+        #td=self.get_dynamic(td)
+
+        
+        #Average  player  unreliability
+        #td=self.get_unreliability(td)
+
+        
+        #Average reliability
+        #td=self.get_reliability(td)
+
+
+        #make ranking columns
+        td = self.get_avg_ranking(td)
+
+        #Make the game type data:
+        #td=self.get_game_type(td)
+
+        return td
